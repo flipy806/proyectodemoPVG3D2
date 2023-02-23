29f006de9ad1b96477be8c7f58fe4e54dc585d50
Felipe Rodriguez <flipy680@gmail.com>
2023-02-22 20:10:34 -0500

    Entrega de tarea

--- Assets/Scripts/adminmove.cs
+++ Assets/Scripts/adminmove.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class adminmove : MonoBehaviour
 {
     public int puntos;
-    public Text textoPuntos;
+    
+    public TMP_Text textoPuntos;
+    public TMP_Text Vidas;
     // Start is called before the first frame update
     void Start()
     {
         puntos = 0;
+        
     }
 
     // Update is called once per frame
@@ -18,6 +22,11 @@ public class adminmove : MonoBehaviour
     {
 
     }
+
+    public void Lifes (int count) {
+        Vidas.text = "x " + count;
+    }
+
     public void sumarpuntos(int score)
     {
         puntos = puntos + score;
--- Assets/Scripts/moving.cs
+++ Assets/Scripts/moving.cs
@@ -8,31 +8,50 @@ public class moving : MonoBehaviour
     private Vector3 pos;
     public float velocidad;
     public float extension;
+   // public Collider Muerte;
+    public Animator Anim;
+    float pos_y;
+    int Punt = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         pos = this.transform.position;
+        Anim = GetComponent<Animator>();
     }
-
+/*
     // Update is called once per frame
     void Update()
     {
-        float pos_x = extension * Mathf.Sin(2 * Mathf.PI * velocidad * Time.time);
+        /*float pos_x = extension * Mathf.Sin(2 * Mathf.PI * velocidad * Time.time);
         pos.x = pos_x;
         this.transform.position = pos;
+        pos.y = pos_y;
+        if( pos_y <= -1){
+            Destroy(gameObject);
+        }
     }
-
-    private void OnCollisionEnter(Collision collision)
+*/
+    private void OnTriggerEnter(Collider Other)
     {
-        Debug.Log("Colision con " + collision.collider.name);
-        this.GetComponent<Animator>().SetFloat("velocidad", 4);
-        this.GetComponent<Animator>().SetInteger("estado", 1);
+        if(Other.gameObject.CompareTag("Player")){
+        Anim.SetInteger("estado", 1);
+        Punt = Punt + 1;
+        Debug.Log("Colision con " + Other.GetComponent<Collider>().name);
+        GameObject.Find("manager").GetComponent<adminmove>().sumarpuntos(1);
+        Destroy(gameObject, 1.5f);
+
+        //this.GetComponent<Animator>().SetFloat("velocidad", 4);
+        }
     }
 
-    public void setEstado(int est)
+/*public IEnumerator Muerto(){
+    yield return new WaitForSeconds(1);
+     Destroy(gameObject);
+}*/
+    /*public void setEstado(int est)
     {
         this.GetComponent<Animator>().SetInteger("estado", est);
 
-    }
+    }*/
 }
